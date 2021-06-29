8ca68c94749ff68ee2b4be160316999d217a8bfb
syhbandi <saboonlbk@gmail.com>
2021-06-29 15:43:11 +0800

    tambah phpexcel

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -62,7 +62,9 @@ class Autoload extends AutoloadConfig
 	 *```
 	 * @var array<string, string>
 	 */
-	public $classmap = [];
+	public $classmap = [
+		'PHPExcel' => APPPATH . 'ThirdParty/PHPExcel/PHPExcel.php',
+	];
 
 	/**
 	 * -------------------------------------------------------------------
